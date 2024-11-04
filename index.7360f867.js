339488f9d14f17beb74bc2e78c85ce2cc8457c61
Igor <igor.olshanskyi@gmail.com>
2024-11-04 11:35:42 +0200

    make build

---
+++ index.7360f867.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=t.clientWidth,i=t.clientHeight,c=e.clientWidth,n=e.clientHeight;t.style.top=c/2-l/2+"px",t.style.left=n/2-i/2+"px";
+//# sourceMappingURL=index.7360f867.js.map
